93a555527d752124b452a8ee33b1d78ac6fd02c1
Sharon Lin <sharon.lin.14@gmail.com>
2015-07-11 16:37:28 -0400

    Updates

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ config.web = {};
 config.middleware = {};
 
 // OpenTok Credentials
-config.opentok.key = process.env.TB_KEY || 'keynast';
-config.opentok.secret=  process.env.TB_SECRET || 'keysec';
+config.opentok.key = process.env.TB_KEY || 'Your opentok key';
+config.opentok.secret=  process.env.TB_SECRET || 'Your opentok secret';
 
 // Database configuration. Do not change if you do not plan to use redis
 config.db.redis = false; // are you using redis?
